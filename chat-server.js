18caf34faf869c86663b6bc97ec67419f7a96f4a
zelataino <gtaino@wustl.edu>
2017-04-02 22:38:06 -0500

    Finished the listing feature

--- chat-server.js
+++ chat-server.js
@@ -50,73 +50,189 @@ app.listen(3456);
 // Do the Socket.IO magic:
 var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
-	// This callback runs when a new Socket.IO connection is established.
-	createRoom("lobby", "xoxox");
-	socket.on('message_to_server', function(data) {
-		// This callback runs when the server receives a new message from the client.
+// This callback runs when a new Socket.IO connection is established.
+createRoom("lobby", "xoxox");
+socket.on('message_to_server', function(data) {
+	// This callback runs when the server receives a new message from the client.
 
-		console.log("message: "+data["room"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client",{message:data["message"], messagewriter:data["messagewriter"], room: data["room"] }) // broadcast the message to other users
-	});
-	socket.on('user_entering', function(data) {
-		console.log("new user: "+data["newuser"]);
-		socketids[socket.id] = data["newuser"];
-		users.push(data["newuser"]);
-		var lobby = data["room"];
-		io.sockets.emit("user_entering",{newuser:socketids, room: "lobby"})
-	});
-	socket.on("create_chat", function(data) {
-		console.log("creator: "+data["creator"]);
-		var name = data["roomname"];
-		console.log("room name: "+data["roomname"]);
-		createRoom(data["roomname"], data["creator"]);
-		//console.log(rooms);``
-		io.sockets.emit("room_created", {roomname: rooms[name]});
+	console.log("message: "+data["room"]); // log it to the Node.JS output
+	io.sockets.emit("message_to_client",{message:data["message"], messagewriter:data["messagewriter"], room: data["room"] }) // broadcast the message to other users
+});
 
-	});
-	socket.on("get_current_rooms", function(data) {
-		var user = data["user"];
-		io.sockets.emit("room_return", {allrooms: rooms, req: user })
-	});
-	socket.on("join_room", function(data) {
-		var roomjoin = data["room"];
-		var user = data["user"];
-		rooms[roomjoin].users.push(user);
-	}
-);
-socket.on("leaving_room", function(data) {
-	var userleave = data["user"];
-	var room = data["room"];
+
+
+
+
+
+
+socket.on("user_logging_in", function(data){
+	var new_user = data["user"];
+	var lobby = data["room"];
+
+	socketids[socket.id] = new_user;
+	
+	//add user to lobby list
+	var lobby_users = rooms[lobby].users;
+	lobby_users.push(new_user);
+
+	console.log("List of users: " + lobby_users);
+
+	//send user list to newuser to display
+	io.sockets.emit("display_list_to_user", {userList: lobby_users, user: new_user});
+
+	//send new user list to all users in lobby
+	io.sockets.emit("send_new_user_list", {room: lobby, userList: lobby_users});
+});
+
+
+
+
+
+
+
+
+socket.on("switching_rooms", function(data){
+	console.log("SWITCHING ROOMS");
+	var user = data["user"];
+	var old_room = data["old_room"];
+	var new_room = data["new_room"];
+
+	//remove user from old room list
+	var old_room_user_list = rooms[old_room].users;
+	var index = old_room_user_list.indexOf(user);
+	old_room_user_list.splice(index, 1);
+
+	//update old room user list
+	io.sockets.emit("send_new_user_list",{room:old_room, userList: old_room_user_list});
+	console.log("List to show on staying user: " + old_room_user_list);
+
+	//add user to new room list
+	var new_room_user_list = rooms[new_room].users;
+	new_room_user_list.push(user);
+	
+	console.log("List to show on to switching user: " + new_room_user_list);
+	//get new room user list to display
+	io.sockets.emit("display_list_to_user", {userList: new_room_user_list, user: user});
+
+	//send new user list to others in same room
+	io.sockets.emit("send_new_user_list", {room: new_room, userList: new_room_user_list, user: user});
 
 });
+
+
+
+
+
+
+
+
+
+
+
+//USER ENTERING
+socket.on('user_entering', function(data) {
+	console.log("USER ENTERING");
+	console.log("new user: "+ data["newuser"]);
+	socketids[socket.id] = data["newuser"];
+	var new_user = data["newuser"];
+	var lobby = data["room"];
+	var users = rooms[lobby].users;
+	//add user to lobby user list
+	users.push(new_user);
+	// var index = users.indexOf('xoxox');
+	// users.splice(index, 1);
+	console.log("Users in lobby: " + rooms[lobby].users);
+	// io.sockets.emit("user_entering",{newuser:socketids, room: "lobby"});
+	io.sockets.emit("user_entered", {newUser: new_user, newUser_list: rooms[lobby].users, room: lobby});
+});
+
 socket.on("get_chatrooms_users", function(data) {
+	console.log("GET CHATROOM USERS");
 	var room = data["room"];
-	console.log(rooms[room]);
-
-	io.sockets.emit("showing_users", {users: rooms[room].users})
+	var curruser = data["user"];
+	console.log(curruser + " went into " + room);
+	console.log("List of users to be displayed in " + room + " : " + rooms[room].users);
+	io.sockets.emit("showing_users", {user: curruser, users: rooms[room].users, room: room});
 });
 
+socket.on("create_chat", function(data) {
+	console.log("NEW ROOM CREATED");
+	var room_name = data["roomname"];
+	var creator = data["creator"];
+	createRoom(room_name, creator);
 
+	io.sockets.emit("room_created", {user: data['creator'], room_name: room_name});
 
-	socket.on("disconnect", function() {
-		console.log("disconnecting from server");
-		console.log(socket.id);
-		var name = socketids[socket.id];
-		delete socketids[socket.id];
-		io.sockets.emit("change_message", {message:name, roomlist:socketids});
-		//io.sockets.emit("userchange", {roomlist:socketids});
+	console.log("creator: "+ data["creator"]);
+	console.log("room name: "+ data["roomname"]);
+});
 
+socket.on("get_current_rooms", function(data) {
+	var user = data["user"];
+	io.sockets.emit("room_return", {allrooms: rooms, req: user })
+});
 
+socket.on("join_room", function(data) {
+	var roomjoin = data["room"];
+	var user = data["user"];
+	console.log(user + " entered new room " + roomjoin);
+	if (rooms[roomjoin].creator != user) {
+		rooms[roomjoin].users.push(user);
+		console.log("List to be displayed " + rooms[roomjoin].users);
+    }
+});
 
-	});
+socket.on("leaving_room", function(data) {
+	console.log("LEAVING ROOM");
+	var user_to_leave = data["user"];
+	var old_room = data["room"];
+	var old_room_users = rooms[old_room].users;
+	console.log("Old users in " + old_room + " : " + old_room_users);
+	
+	for(i=0; i<old_room_users.length; i++){
+		var a_user = old_room_users[i];
+		if(user_to_leave == a_user){
+			old_room_users.splice(i, 1);
+		}
+	}
+	console.log(user_to_leave);
+	console.log(old_room);
+	io.sockets.emit("user_left_room", {user: user_to_leave, room: old_room});
+	console.log("new users in " + old_room + " : " + rooms[old_room].users);
+
+});
+
+socket.on("disconnect", function() {
+        console.log("disconnecting from server");
+        console.log(socket.id);
+        var name = socketids[socket.id];
+        delete socketids[socket.id];
+        if (typeof name != "undefined") {
+          io.sockets.emit("user_left_message", {message:name, roomlist:socketids});
+          io.sockets.emit("left-chatroom", {name: name});
+          for (var room in rooms) {
+              var index = rooms[room].users.indexOf(name);
+              if (index > -1) {
+                  rooms[room].users.splice(index, 1);
+              }
+              var lobby_users = rooms[room].users;
+              io.sockets.emit("send_new_user_list", {room: room, userList: lobby_users, user: name});
+          }
+        }
 });
 
 function createRoom(roomname, maker) {
 	var room = {
 		roomname: roomname,
 		creator: maker,
-		users: [maker]
+		users: []
 
 	}
-	rooms[roomname] = room
+
+	if (!(roomname in rooms)) {
+		rooms[roomname] = room;
+	}
 }
+
+
+});
--- chatFunctions.js
+++ chatFunctions.js
@@ -1,5 +1,5 @@
 var users = [];
-var currentuser;
+var currentuser = null;
 var socketio = io.connect();
 var currentroom;
 
@@ -36,31 +36,86 @@ socketio.on("message_to_client", function(data) {
 });
 
 //someone quits window
-socketio.on("change_message", function(data) {
-  var messagecont = document.createElement("div");
-  messagecont.setAttribute("id", "message-container");
-  var messagediv = document.createElement("div");
-  messagediv.setAttribute("class", "a-message");
-  var name = document.createElement("h4");
-  var msg = document.createElement("p");
-  name.innerHTML = data["message"];
-  msg.innerHTML = data["message"] + " has left the room";
-  messagediv.appendChild(name);
-  messagediv.appendChild(msg);
-  document.getElementById("message-container").appendChild(messagediv);
-
+socketio.on("user_left_message", function(data) {
+  if(currentuser != 'undefined'){
+    var messagecont = document.createElement("div");
+    messagecont.setAttribute("id", "message-container");
+    var messagediv = document.createElement("div");
+    messagediv.setAttribute("class", "a-message");
+    var name = document.createElement("h4");
+    var msg = document.createElement("p");
+    msg.style.color = "gray";
+    msg.style.fontStyle = "italic";
+    name.innerHTML = data["message"];
+    msg.innerHTML = data["message"] + " has left the room";
+    messagediv.appendChild(name);
+    messagediv.appendChild(msg);
+    document.getElementById("message-container").appendChild(messagediv);
+    console.log(data["roomlist"]);
+  }
 });
 
 //creates a chatroom
 socketio.on("room_created", function(data) {
-  var name = data["roomname"];
-  console.log(name.roomname);
-  createRoomElement(name.roomname);
-  console.log(data["roomname"]);
+  var room_name = data["room_name"];
+  var user = data["user"];
+  
+  createRoomElement(room_name);
+  
+  //if user created room, switch room
+  if (user == currentuser) {
+    switchRooms(room_name);
+  }
 });
 
-socketio.on("room_return", function(data) {
+//create room
+function createRoomElement (roomname) {
+  var li = document.createElement("li");
+  var span = document.createElement("span");
+  span.setAttribute("id", roomname);
+  span.setAttribute("class", "rooms");
+  span.innerHTML = "# " + roomname;
+  li.appendChild(span);
+  document.getElementById("chatrooms").appendChild(li);
+}
 
+//when user switches room
+function switchRooms(room_name){
+  var old_room = currentroom;
+  currentroom = room_name;
+
+  socketio.emit("switching_rooms", {user: currentuser, old_room: old_room, new_room: room_name});
+
+  //room name changes
+  document.getElementById("chat-channel").innerHTML = "# " + currentroom;
+
+  // socketio.emit("leaving_room", {user: currentuser, room: old_room});
+  // socketio.emit("join_room", {user: currentuser, room: currentroom});
+  // socketio.emit("get_chatrooms_users", {user: currentuser, room: currentroom});
+  $("#login-view.fullscreen-view").hide();
+  $(".wrapper").show();
+}
+
+// when user switches room 
+$(document).on("click", ".rooms", function() {
+  switchRooms(event.target.id);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+socketio.on("room_return", function(data) {
   var rooms = data["allrooms"];
   console.log(rooms);
   var numrooms = rooms.length;
@@ -83,63 +138,186 @@ function sendMessage(){
    socketio.emit("message_to_server", {message:msg, messagewriter:usr, room: currentroom});
 }
 
-function addUser(username)  {
+function createDmElement(username){
   var li = document.createElement("li");
+  li.setAttribute("class","a_user");
+  li.setAttribute("id",username);
+  var span = document.createElement("span");
   var circle = document.createElement("div");
   circle.setAttribute("id", "circle");
-  li.appendChild(circle);
-  li.innerHTML = username;
-  console.log(username);
-
-}
-
-function createRoomElement (roomname) {
-  var li = document.createElement("li");
-  var span = document.createElement("span");
-  span.setAttribute("id", roomname);
-  span.setAttribute("class", "rooms");
-  span.innerHTML = "# " + roomname;
+  span.appendChild(circle);
+  var usernameDiv = document.createElement("div");
+  usernameDiv.setAttribute("id", "username-div");
+  usernameDiv.innerHTML = username;
+  // span.appendChild(username);
+  span.appendChild(usernameDiv);
   li.appendChild(span);
-  document.getElementById("chatrooms").appendChild(li);
-
+  document.getElementById("directmessages").appendChild(li);
 }
 
 
-$(document).on("click", ".rooms", function() {
-  var leaveroom = currentroom;
-  socketio.emit("leaving_room", {user: currentuser, room: leaveroom});
-  console.log(leaveroom);
-  console.log("room func");
-  currentroom = event.target.id;
-  console.log(currentroom);
-  socketio.emit("join_room", {user: currentuser, room: currentroom});
-  document.getElementById("chat-channel").innerHTML = "# " + currentroom;
-  $("#login-view.fullscreen-view").hide();
-  $(".wrapper").show();
+
+// Login screen
+$("#login_name").keyup(function(e){
+  var code = (e.keyCode ? e.keyCode : e.which);
+  if(code == 13){
+    var user = document.getElementById("login_name").value;
+    currentuser = user;
+    socketio.emit("get_current_rooms", {user: currentuser});
+    currentroom = "lobby";
+
+    socketio.emit("user_logging_in", {user: user, room: currentroom});
+
+    //socketio.emit("user_entering", {newuser: user, room: currentroom});
+    //socketio.emit("get_chatrooms_users", {room: currentroom, user: currentuser});
+
+
+    $("#add-chatroom-view.fullscreen-view").hide();
+    $("#login-view.fullscreen-view").fadeOut();
+    $(".wrapper").show();
+    document.getElementById('username').innerHTML = user;
+  }
 });
 
+
+
+
+
+//after loggin, display lobby list
+socketio.on("display_list_to_user", function(data){
+  console.log("DISPLAY NEW LIST");
+  var user_room_list = data["userList"];
+  var user = data["user"];
+
+  console.log("display list to entering user: " + user_room_list);
+  console.log(user_room_list.length);
+
+  if(user == currentuser){
+    //remove from old list
+    var old_elems = document.getElementsByClassName("a_user");
+    while(old_elems.length > 0){
+      old_elems[0].parentNode.removeChild(old_elems[0]);
+    }
+
+     //create new list
+    for(i=0; i<user_room_list.length; i++){
+      createDmElement(user_room_list[i]);
+    }
+  }
+});
+
+//if someone entered my chat room, remove and update list
+socketio.on("send_new_user_list",function(data){
+  console.log("SEND NEW USER LIST");
+  var user_list = data["userList"];
+  var room = data["room"];
+  var user = data["user"];
+
+  console.log("for room " + room);
+  console.log("new list:" + user_list);
+  // if(user != currentuser){
+    if(currentroom == room){
+
+      //remove old list
+      var old_elems = document.getElementsByClassName("a_user");
+
+      console.log(old_elems);
+      console.log(old_elems.length);
+
+      while(old_elems.length > 0){
+        old_elems[0].parentNode.removeChild(old_elems[0]);
+      }
+
+      //create new list
+      for(i=0; i<user_list.length; i++){
+        console.log("Element to be printed "+user_list[i]);
+        createDmElement(user_list[i]);
+      }
+    }
+});
+
+
+
+
+
+
+//other ppl entered room
+socketio.on("user_entered", function(data){
+  if(currentuser != null){
+    console.log("USER ENTERED");
+    var new_user = data["newUser"];
+    var newUser_list = data["newUser_list"];
+    var room_entered = data["room"];
+    // if(new_user != currentuser){
+    update_dm_users(new_user, newUser_list, room_entered);
+    // }
+  }
+
+});
+
+//i enterd room, it updates dm list
+socketio.on("showing_users", function(data){
+  if(currentuser!=null){  
+    console.log("SHOWING USERS");
+    var user = data["user"];
+    var newUser_list = data["users"];
+    var room_entered = data["room"];
+    update_dm_users(user, newUser_list, room_entered);
+  }
+});
+
+
+
+function update_dm_users(new_user, user_list, room){
+  console.log("UPDATE DM USERS");
+  if(room == currentroom){
+    console.log("CLEAR USERS");
+    //clear DM list
+    var parent = document.getElementById("directmessages");
+    var elements = document.getElementsByClassName("a_user");
+    // console.log(elements);
+    console.log(elements.length);
+    console.log(elements);
+    if(elements.length != 0){
+      for(i=0;i<elements.length;i++){
+        console.log("Removing: " + elements[i].id);
+        parent.removeChild(elements[i]);
+      }
+    }
+
+    console.log("ADD USER LIST");
+    //creates list
+    console.log("User list: " + user_list);
+    for(i=0; i < user_list.length; i++){
+      createDmElement(user_list[i]);
+    }
+    console.log("-------------------");
+  }
+}
+
+socketio.on("user_left_room", function(data){
+  var user_who_left = data["user"];
+  var old_room = data["room"];
+  var element = document.getElementById(user_who_left);
+  var parent = document.getElementById("directmessages");
+  console.log(element.id);
+  parent.removeChild(element);
+});
 
 
 $(document).on("click", "#add-chatroom-btn", function(){
   $("#add-chatroom-view").fadeIn();
 });
 
-$(document).on("click", "#chat-channel-btn", function() {
-  socketio.emit("get_chatrooms_users", {room:currentroom});
-  $(".wrapper").hide();
-  $("#sidePanel").hide();
-  $("#userview").show();
-
-});
 
 $("#chatroom-title").keyup(function(e) {
-  var newroomname = document.getElementById("chatroom-title").value;
   var code = (e.keyCode ? e.keyCode : e.which);
   if(code == 13){
-    // console.log(newroomname);
+    var newroomname = document.getElementById("chatroom-title").value;
     if (newroomname.length > 2) {
-    //  console.log(currentuser);
      socketio.emit("create_chat", {creator: currentuser, roomname:newroomname});
+
+     //update ui
      $("chatroom-title").val("");
      $("#add-chatroom-view.fullscreen-view").fadeOut();
      $(".wrapper").show();
@@ -168,28 +346,6 @@ $("#message-textarea").keyup(function(e){
   }
 });
 
-
-
-// Login screen
-$("#login_name").keyup(function(e){
-//  console.log("");
-  var code = (e.keyCode ? e.keyCode : e.which);
-  if(code == 13){
-    socketio.emit("get_current_rooms", {user: currentuser});
-    var user = document.getElementById("login_name").value;
-    currentuser = user;
-    socketio.emit("get_current_rooms", {user: currentuser});
-    $("#add-chatroom-view.fullscreen-view").hide();
-    $("#login-view.fullscreen-view").fadeOut();
-    $(".wrapper").show();
-    currentroom = "lobby";
-    // console.log(currentroom);
-    // console.log(currentuser);
-    socketio.emit("user_entering", {newuser: user, room: currentroom});
-    document.getElementById('username').innerHTML = user;
-
-  }
-});
 
 
 $(document).on("click", ".add-chatroom-btn", function () {
